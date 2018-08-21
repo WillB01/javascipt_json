76e29cf4f610ac662218985c5e76bd3f5ae9274e
WillB01 <wb.william@outlook.com>
2018-08-21 22:17:56 +0200

    3

---
+++ script.js
@@ -0,0 +1,11 @@
+let info = {
+    "full_name" : "Bengt Arne",
+    "title" : "VD",
+    "links": [
+        {"blog1" : "lorem lorem lorem1"},
+        {"blog2" : "lorem lorem lorem2"},
+        {"blog3" : "lorem lorem lorem3"},
+        {"blog4" : "lorem lorem lorem4"},
+        {"blog5" : "lorem lorem lorem5"},
+    ]
+}
